8d5815224ad4c48eab47a63fc0fb889ed8848fcc
fadill980 <akungithub878@gmail.com>
2024-03-19 22:05:23 +0700

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,12 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
-
+  info: any = {}
+  saveData() {
+    console.log(this.info);
+  }
+  constructor(private route: Router) {}
+  scorePage() {
+    this.route.navigate(['/score']);
+  }
 }
